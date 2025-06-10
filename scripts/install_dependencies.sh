1681c453724072624e8a822a2169a93f24ba3b6c
Rishav-anand <anandrishav186@gmail.com>
2025-06-10 10:57:36 +0530

    Create install_dependencies.sh

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "[*] Checking Python3..."
+if ! command -v python3 &> /dev/null
+then
+    echo "[*] Python3 not found. Installing..."
+    sudo yum update -y
+    sudo yum install python3 -y
+else
+    echo "[✓] Python3 already installed."
+fi
+
+echo "[*] Checking pip3..."
+if ! command -v pip3 &> /dev/null
+then
+    echo "[*] pip3 not found. Installing..."
+    sudo dnf install python3-pip -y
+else
+    echo "[✓] pip3 already installed."
+fi
+
+echo "[*] Installing boto3 and requests..."
+pip3 install --user requests boto3
+
+echo "[✓] All dependencies installed."
+
+
