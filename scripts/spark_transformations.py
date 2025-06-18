54a6145344a5c9739f804ffec2b11f142bed1bf0
Rishav-anand <anandrishav186@gmail.com>
2025-06-18 20:41:37 +0530

    Update spark_transformations.py

--- scripts/spark_transformations.py
+++ scripts/spark_transformations.py
@@ -62,6 +62,9 @@ try:
     logger.info("Showing rates_df dataframe..")
     rates_df.show()
 
+    #Incremental_load
+    txn_df = txn_df.filter(col('Date') == date.today())
+
     # Step 1: Ensure txn_df's date is a string
     txn_df = txn_df.withColumn("Date", col("Date").cast("string"))
 
