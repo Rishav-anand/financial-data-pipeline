84465ecd113e5dc2e886ee64ed68db2fd82f0665
Rishav-anand <anandrishav186@gmail.com>
2025-06-19 09:40:11 +0530

    Update spark_transformations.py

--- scripts/spark_transformations.py
+++ scripts/spark_transformations.py
@@ -61,6 +61,10 @@ try:
     # Step 3: Create DataFrame
     rates_df = spark.createDataFrame(flattened_data, schema=schema)
 
+     # Show the DataFrame before filter
+    logger.info("Showing txn_df dataframe..")
+    txn_df.show()
+
     #Incremental_load
     txn_df = txn_df.filter(col('Date') == today)
 
